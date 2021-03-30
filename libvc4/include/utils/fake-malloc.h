0ea2f81095bd96e43c38d45d9f194b6f0d3d780a
Anichang <20516565+mfp20@users.noreply.github.com>
2021-03-30 12:40:22 +0200

    temporary remove libvc4

--- libvc4/include/utils/fake-malloc.h
+++
@@ -1,4 +0,0 @@
-#ifndef HAS_DYNAMIC_ALLOCATIONS
-static inline void* malloc(size_t size) { panic("malloc: dynamic allocations not supported on this configuration"); }
-static inline void free(void* obj)      { panic("free: dynamic allocations not supported on this configuration"); }
-#endif
